b52054bcfc81dd55212eb58aa511889137cdc604
Emir Magalov <emir.mahalo@gmail.com>
2026-04-22 21:19:37 +0400

    first commit

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,3 +1,3 @@
-export const apiUrl = "http://127.0.0.1:8000/api/v1/"
+export const apiUrl = import.meta.env.VITE_API_URL || "http://localhost:8000/api/v1/";
 
 export const publishableKey = "pk_test_51TO7LZCjGv4RLZGlcAMVO9iBbRpFgLjeTdDEecZ21kWBgWP0svBbQJuH3lzyctYfdph58qjg5foM2rcdFPrAkdOI00zgOLY9O6"
