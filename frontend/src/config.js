9e1538ca2c2fa9aedb23c28c252271bf6fa3d3c1
Emir Magalov <emir.mahalo@gmail.com>
2026-04-23 14:06:26 +0400

    format

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,3 +1,3 @@
-export const apiUrl = "https://alluring-manifestation-production-bfab.up.railway.app/api/v1/" || "http://localhost:8000/api/v1/";
+export const apiUrl = "https://stripetest-production-1be8.up.railway.app/api/v1/" || "http://localhost:8000/api/v1/";
 
 export const publishableKey = "pk_test_51TO7LZCjGv4RLZGlcAMVO9iBbRpFgLjeTdDEecZ21kWBgWP0svBbQJuH3lzyctYfdph58qjg5foM2rcdFPrAkdOI00zgOLY9O6"
