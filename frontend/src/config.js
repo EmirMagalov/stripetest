5cbbdfb4834670062d6ebdea6db74e9defaf798c
Emir Magalov <emir.mahalo@gmail.com>
2026-04-23 13:54:43 +0400

    format

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,3 +1,3 @@
-export const apiUrl = import.meta.env.VITE_API_URL
+export const apiUrl = "https://alluring-manifestation-production-bfab.up.railway.app/api/v1/" || "http://localhost:8000/api/v1/";
 
 export const publishableKey = "pk_test_51TO7LZCjGv4RLZGlcAMVO9iBbRpFgLjeTdDEecZ21kWBgWP0svBbQJuH3lzyctYfdph58qjg5foM2rcdFPrAkdOI00zgOLY9O6"
