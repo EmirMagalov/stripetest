98ad0b23c708e871a24d84f6ef7716b4ba4e7545
Emir Magalov <emir.mahalo@gmail.com>
2026-04-25 00:25:39 +0400

    media

--- backend/stripe_integration_tz/stripe_integration_tz/settings.py
+++ backend/stripe_integration_tz/stripe_integration_tz/settings.py
@@ -141,10 +141,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = "static/"
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 MEDIA_ROOT = "/app/media"
 MEDIA_URL = "/media/"
--- backend/stripe_integration_tz/stripe_integration_tz/urls.py
+++ backend/stripe_integration_tz/stripe_integration_tz/urls.py
@@ -21,6 +21,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import re_path
 from django.views.static import serve
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("store.urls")),
@@ -29,7 +30,7 @@ urlpatterns = [
 
 if not settings.DEBUG:
     urlpatterns += [
-        re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+        re_path(r"^media/(?P<path>.*)$", serve, {"document_root": settings.MEDIA_ROOT}),
     ]
 else:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
